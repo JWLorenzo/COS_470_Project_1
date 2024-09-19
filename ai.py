88b100b70b68bb66e0042cb1217b9a0f00739f26
JacobWLorenzo <jacob.w.lorenzo@maine.edu>
2024-09-19 17:13:34 -0400

    New Branch Who Dis

--- ai.py
+++ ai.py
@@ -120,8 +120,6 @@ class AI:
         The same goes for goal hexes (0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
         """
 
-        """First, let's make it not hit walls"""
-
         return self.depth_first_search(percepts)
 
 
